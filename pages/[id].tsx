99d9c692f406e20f027ddc3a228412b760c51399
ynchen2829 <ynchen2829@gmail.com>
2023-01-29 02:36:43 -0600

    fixed % dom reading issue

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,6 @@
 import { google } from 'googleapis';
 import { useRouter } from 'next/router';
+import {Nav} from './layout';
 
 export  async function getServerSideProps({query}) {
     // authorization
@@ -33,21 +34,42 @@ export  async function getServerSideProps({query}) {
     };
   }
 
+function pad(n: string,length: number){
+  var len = length - (''+n).length;
+  return (len > 0 ? new Array(++len).join('0') : '') + n
+}
 export default function Details({data}){
     const router = useRouter();
     const row = Number(router.query.info); //row number of the user's information
     const name = data[row][0];//grab the user's name for greeting message
-    const lottery_id = data[row][6]; //grab the user's lottery number
+    const lottery_id = pad(data[row][6],3); //grab the user's lottery number and convert to 3 digits
+
+    const d0 = lottery_id.charAt(0);
+    const d1 = lottery_id.charAt(1);
+    const d2 = lottery_id.charAt(2);
 
     //console.log(data[row]);
     return(
-        <div className='container'>
-            <h1>您好 {name}</h1>
-            {lottery_id ?
-                (<h2>您的抽奖号码是： {lottery_id} </h2>) 
-                : (<h2>暂时没有抽奖号喔～</h2>)
-            }
+      <div className='bg-gradient-to-tl from-rose-400 to-orange-300 h-screen flex flex-col items-center justify-center'>
+        <Nav></Nav>
+        <iframe src="https://giphy.com/embed/iehOstHSrp1XUYvXFZ" width="480" height="219" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+        <h1 className="text-center text-3xl font-medium text-white mt-12 pt-0">您好 {name},</h1>
+        <h1 className="text-center text-2xl font-medium text-white mt-1 pb-10">Welcome, {name}</h1>
+        {lottery_id ?
+            (<div>
+              <h2 className="text-center text-2xl font-medium text-white mt-4">您的抽奖号码是：</h2>
+              <h2 className="text-center text-xl font-medium text-white pb-3">Your lottery number is: </h2>
+              <div className="flex gap-3 justify-center">
+                <div className="font-mono text-9xl bg-slate-200 shadow-inner rounded p-2 text-center opacity-60">{d0}</div>
+                <div className="font-mono text-9xl bg-slate-200 shadow-inner rounded p-2 text-center opacity-60">{d1}</div>
+                <div className="font-mono text-9xl bg-slate-200 shadow-inner rounded p-2 text-center opacity-60">{d2}</div>
+              </div>
+            </div>
             
-        </div>
+            ) 
+            : (<h2 className="text-center text-xl font-medium text-white mt-4">暂时没有抽奖号喔～</h2>)
+        }
+      </div>
+
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { use, useState } from 'react';
 import { google } from 'googleapis';
 import { useRouter } from 'next/router';
+import {Nav} from './layout';
 
 export  async function getServerSideProps() {
   // auth
@@ -25,6 +26,7 @@ export  async function getServerSideProps() {
 export default function Home({ data }) {
   const [input, setValue] = useState('');
   const [match, setMatch] = useState(true);
+  const [pressed, setPressed] = useState(false);
   const router = useRouter();
   let redirect_url = "/";
 
@@ -36,31 +38,42 @@ export default function Home({ data }) {
     e.preventDefault();
 
     console.log(data);
-
+    setPressed(true);
     //check match for lottery number, if found then redirect
     for (let i = 0; i < data.length; i++) {
       if (data[i][0] === input) {
         setMatch(true);
         redirect_url += input;
+        redirect_url = redirect_url.replaceAll('%',',');
+        console.log(redirect_url);
         router.push({
           pathname: redirect_url,
           query:{
-            info: (i+2)
+            info: (i)
           }
         });
+        break;
+      }
+      if(i == data.length - 1){
+        setMatch(false);
       }
     }
 
-    setMatch(false);
   };
 
   return (
-    <div className='bg-gradient-to-tl from-rose-400 to-orange-300 h-screen flex justify-center items-center'>
+    <div className='bg-gradient-to-tl from-rose-400 to-orange-300 h-screen flex flex-col justify-center'>
+      <Nav></Nav>
+      <h1 className="text-center text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r to-amber-600 from-red-700 mt-12 pt-0">欢迎来到UTCSSA兔年新春晚会！</h1>
+      <h1 className="text-center text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r to-amber-600 from-red-700 mt-1 pb-10">Welcome to the UTCSSA Spring Gala!</h1>
       <div className="container w-full max-w-xs bg-white bg-opacity-50 mx-auto shadow-md">
-        <form className="justify-center items-center rounded px-8 pt-6 pb-8 mb-4">
+        <form className="justify-center items-center rounded px-8 pt-6 pb-4" onSubmit={handleSubmit}>
             <div className="mb-4">
-              <label className=" block text-gray-700 text-lg font-bold mb-2 pb-2">
+              <label className=" block text-gray-700 text-lg font-bold pb-2">
               请输入4位数邀请码
+              </label>
+              <label className=" block text-gray-700 text-sm font-bold mb-2 pb-2">
+              Input 4-digit Invitation Code
               </label>
               <input className="shadow appearance-none border py-2 px-3 leading-tight focus:outline-none focus:shadow-outline rounded-lg p-2 text-xl text-red-800 w-48 outline outline-offset-2 outline-2 outline-red-800"
               type="text"
@@ -71,18 +84,34 @@ export default function Home({ data }) {
             </div>
             <div className="flex items-center justify-between">
               <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                提交
+                Submit
               </button>
-              <a className="inline-block align-baseline font-bold text-sm text-red-900 hover:text-red-600" href="#">
-                没有注册？点击这里
-              </a>
             </div>
         </form>
-        {match ? (
-          <p></p>
-        ) : (
-          <p className="text-red-500 text-center">没有发现您的记录捏🤏</p>
-        )}
+        {pressed? (
+          <div>
+            {match ? (
+              <div>
+              <p className="text-green-500 text-center text-sm ">签到成功！跳转中……</p>
+              <p className="text-green-500 text-center mb-3 text-sm ">Success! Redirecting...</p>
+              </div>
+            ) : (
+              <div>
+                <p className="text-red-500 text-center text-sm ">没有发现您的记录捏🤏</p>
+                <p className="text-red-500 text-center mb-3 text-sm ">Your record was not found.</p>
+              </div>
+            )}
+          </div> 
+          ):(<p></p>)
+        }
+        
+      </div>
+      <div className="container w-full max-w-xs mx-auto my-2">
+        <a className="inline-block align-baseline font-bold text-sm text-red-900 hover:text-red-600 mt-3 mx-auto" href="#">
+                没有注册？点击这里。
+                <br></br>
+                Not registered? Click here.
+        </a>
       </div>
     </div>
   );
